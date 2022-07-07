82ffa841d06a244ad67dd9c79e8f2f3957c18582
carnivalgoblin <rico.prodan@gmail.com>
2022-07-07 17:27:34 +0200

    added test for warning

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -17,13 +17,22 @@ describe('<NumberOfEvents /> component', () => {
     expect(NumberOfEventsWrapper.find('.number-of-events').prop('value')).toBe(number);
   });
 
-  test('change state when text input changes', () => {
+  test('change state when number input changes', () => {
     NumberOfEventsWrapper.setState({
       numberOfEvents: 32
     });
     const eventObject = { target: { value: 16 } };
     NumberOfEventsWrapper.find('.number-of-events').simulate('change', eventObject);
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(16);
+  });
+
+  test('change state when number input too high', () => {
+    NumberOfEventsWrapper.setState({
+      numberOfEvents: 32
+    });
+    const eventObject = { target: { value: 50 } };
+    NumberOfEventsWrapper.find('.number-of-events').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('warningText')).toBe('Please choose a number between 1 and 32');
   });
 
 });
