7a3b067e7bcfcdf94dbf4ad791f94cb0ea0b5cbc
carnivalgoblin <rico.prodan@gmail.com>
2022-07-07 14:25:49 +0200

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
+import EventList from "../EventList";
+import CitySearch from "../CitySearch";
 
 describe('<App /> component', () => {
-  test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
+  test('render EventList', () => {
     expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 });
