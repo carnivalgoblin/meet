d399ca4aac0e9cd9987f53d01b85634f888dcde4
carnivalgoblin <rico.prodan@gmail.com>
2022-07-08 10:41:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CitySearch from './CitySearch';
 import EventList from './EventList';
 import NumberOfEvents from './NumberOfEvents';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <EventList />
+        <NumberOfEvents />
+      </div>
+    );
+  }
 }
 
 export default App;
