1ae71cc1f38c042ac39460194de107486f208869
carnivalgoblin <rico.prodan@gmail.com>
2022-07-11 13:21:41 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -51,7 +51,7 @@ const getToken = async (code) => {
   try {
     const encodeCode = encodeURIComponent(code);
 
-    const response = await fetch(`https://z0ka8x6vp9.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`);
+    const response = await fetch('https://z0ka8x6vp9.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
